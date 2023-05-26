89ef6d481f9a2dde9c76b5dc34889a56398f9e1a
Divxsh <guptadivash70@gmail.com>
2023-05-26 23:02:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import ReferAndEarn from "./pages/ReferAndEarn/ReferAndEarn";
+import FriendsReferred from "./pages/ReferAndEarn/FriendsReferred";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<div className='App'>
+				<Navbar />
+				<Routes>
+					<Route path='/' element={<ReferAndEarn />} />
+					<Route path='friendsreferred' element={<FriendsReferred />} />
+					{/* <Route path='*' element={<NoPage />} /> */}
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
